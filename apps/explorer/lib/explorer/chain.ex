85f58458927750be074ed5476ef47c3ad8ba2883
nikitosing <nikitosing4@mail.ru>
2022-02-08 15:57:40 +0300

    Fix small bug

--- apps/explorer/lib/explorer/chain.ex
+++ apps/explorer/lib/explorer/chain.ex
@@ -3221,7 +3221,6 @@ defmodule Explorer.Chain do
     fetched_transactions =
       if paging_options.page_number == 1 do
         paging_options.page_size
-        |> Kernel.+(1)
         |> Transactions.take_enough()
         |> case do
           nil ->
